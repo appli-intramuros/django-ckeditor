d99ea09a4dad49d96e4e3a5e0cdec4fb59e1da8f
Antoine Lascou <antoine.lascou@epsi.fr>
2024-11-13 12:06:11 +0100

    migration vers django 4

--- ckeditor_demo/urls.py
+++ ckeditor_demo/urls.py
@@ -1,16 +1,17 @@
 from __future__ import absolute_import
 
 from django.conf import settings
-from django.conf.urls import include, url
+from django.conf.urls import include
+from django.urls import re_path
 from django.conf.urls.static import static
 from django.contrib import admin
 
 from .demo_application.views import ckeditor_form_view
 
 urlpatterns = [
-    url(r'^$', ckeditor_form_view, name='ckeditor-form'),
-    url(r'^admin/', admin.site.urls),
-    url(r'^ckeditor/', include('ckeditor_uploader.urls')),
+    re_path(r'^$', ckeditor_form_view, name='ckeditor-form'),
+    re_path(r'^admin/', admin.site.urls),
+    re_path(r'^ckeditor/', include('ckeditor_uploader.urls')),
 ] + static(
     settings.STATIC_URL,
     document_root=settings.STATIC_ROOT
--- ckeditor_uploader/forms.py
+++ ckeditor_uploader/forms.py
@@ -1,10 +1,7 @@
 from __future__ import absolute_import
 
 from django import forms
-try:
-    from django.utils.translation import gettext_lazy as _
-except ImportError:
-    from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import gettext_lazy as _
 
 
 class SearchForm(forms.Form):
